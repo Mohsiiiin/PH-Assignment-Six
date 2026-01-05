4a36f82b8b398db59ad8299971b89184c23d31b2
mohsiiiin <ahmedmohsiiiin@gmail.com>
2026-01-05 18:52:41 +0600

    category Load Tree Categories dynamically on the left side added

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,21 @@
-console.log('hyyy')
+const loadCetagory = () => {
+    const url = "https://openapi.programming-hero.com/api/categories"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategory(data.categories))
+}
+
+const displayCategory = (categories) => {
+    const catCategory = document.getElementById("left-side");
+    catCategory.innerHTML = "";
+
+    for (let category of categories) {
+        const leftCategory = document.createElement("div")
+        leftCategory.innerHTML =`
+        <button  class="btn justify-start btn-block shadow flex-1 ">${category.category_name} </button>`;
+
+        catCategory.append(leftCategory)
+    }
+}
+
+loadCetagory();
